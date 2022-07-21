87130257efaea0d59f6de4f26ad780e3b7c54085
MAYO <marca1325@gmail.com>
2022-07-21 11:29:11 +0200

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetsComponent } from './components/pets/pets.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetsComponent
+    PetsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
